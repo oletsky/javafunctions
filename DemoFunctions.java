7171c1abbe09853d20f759b69d6f858326fb2389
Oleksiy Oletsky <oletsky@ukr.net>
2020-05-03 20:35:01 +0300

    Add files via upload

---
+++ DemoFunctions.java
@@ -0,0 +1,14 @@
+package javatraining.oletsky.javafunctions;
+
+public class DemoFunctions {
+    public static void main(String[] args) {
+        int q = 5;
+        int res = kvadr(q);
+        System.out.println(res);
+    }
+
+    static int kvadr(int n) {
+        return n * n;
+    }
+
+}
